61d46e03e111ac867be8621e0be320c4b85e2cd5
supertanuki <supertanuki@gmail.com>
2025-02-14 14:51:23 +0100

    Adjust framerates

--- src/Sprites/HeroAnims.js
+++ src/Sprites/HeroAnims.js
@@ -1,43 +1,47 @@
+const params = new URLSearchParams(window.location.search);
+const moveframerate = params.get("moveframerate") || 10
+const idleframerate = params.get("idleframerate") || 4
+
 export const createHeroAnims = function(anims) {
 	anims.create({
 		key: 'mai-walk-down',
 		frames: anims.generateFrameNames('mai', { start: 1, end: 3, prefix: 'walk-down-' }),
 		repeat: -1,
-		frameRate: 14
+		frameRate: moveframerate,
 	});
 
 	anims.create({
 		key: 'mai-walk-up',
 		frames: anims.generateFrameNames('mai', { start: 1, end: 3, prefix: 'walk-up-' }),
 		repeat: -1,
-		frameRate: 14
+		frameRate: moveframerate,
 	});
 
 	anims.create({
 		key: 'mai-walk-side',
 		frames: anims.generateFrameNames('mai', { start: 1, end: 3, prefix: 'walk-right-' }),
 		repeat: -1,
-		frameRate: 14
+		frameRate: moveframerate,
 	});
 
 	anims.create({
 		key: 'mai-idle-down',
 		frames: anims.generateFrameNames('mai', { start: 1, end: 3, prefix: 'idle-down-' }),
 		repeat: 0,
-		frameRate: 4
+		frameRate: idleframerate,
 	});
 
 	anims.create({
 		key: 'mai-idle-up',
 		frames: anims.generateFrameNames('mai', { start: 1, end: 3, prefix: 'idle-up-' }),
 		repeat: 0,
-		frameRate: 4
+		frameRate: idleframerate,
 	});
 
 	anims.create({
 		key: 'mai-idle-side',
 		frames: anims.generateFrameNames('mai', { start: 1, end: 3, prefix: 'idle-right-' }),
 		repeat: 0,
-		frameRate: 4
+		frameRate: idleframerate,
 	});
 }
