47c92535472b3a16f8810a6f0d00b6d2515394a1
supertanuki <supertanuki@gmail.com>
2025-03-07 18:46:59 +0100

    Exclam anim

--- src/UI/Chat.js
+++ src/UI/Chat.js
@@ -1,23 +1,50 @@
 import Phaser from "phaser";
 
 export default class Chat extends Phaser.Physics.Arcade.Sprite {
-  constructor(scene, x, y, texture, frame, chatIconDeltaX, chatIconDeltaY, disableChatIcon) {
+  constructor(
+    scene,
+    x,
+    y,
+    texture,
+    frame,
+    chatIconDeltaX,
+    chatIconDeltaY,
+    disableChatIcon
+  ) {
     super(scene, x, y, texture, frame);
 
-    this.chatIconDeltaX = chatIconDeltaX || 0
-    this.chatIconDeltaY = chatIconDeltaY || 0
-    this.disableChatIcon = disableChatIcon
+    scene.anims.create({
+      key: "exclam",
+      frames: this.anims.generateFrameNames("sprites", {
+        start: 2,
+        end: 3,
+        prefix: "exclam-",
+      }),
+      repeat: -1,
+      frameRate: 3,
+    });
 
-    this.chatImageUi = scene.add.image(this.x + this.chatIconDeltaX, this.y - 13 + this.chatIconDeltaY, 'sprites', 'exclam-3');
-    this.chatImageUi.setVisible(false)
-    this.chatImageUi.setDepth(1000)
+    this.chatIconDeltaX = chatIconDeltaX || 0;
+    this.chatIconDeltaY = chatIconDeltaY || 0;
+    this.disableChatIcon = disableChatIcon;
+
+    this.chatImageUi = scene.add
+      .sprite(
+        this.x + this.chatIconDeltaX,
+        this.y - 13 + this.chatIconDeltaY,
+        "sprites",
+        "exclam-3"
+      )
+      .setDepth(1000)
+      .setVisible(false);
+    this.chatImageUi.anims.play("exclam", true);
   }
-  
+
   stopChatting() {
-    this.chatImageUi.setVisible(false)
+    this.chatImageUi.setVisible(false);
   }
 
   readyToChat() {
-    this.chatImageUi.setVisible(!this.disableChatIcon)
+    this.chatImageUi.setVisible(!this.disableChatIcon);
   }
 }
