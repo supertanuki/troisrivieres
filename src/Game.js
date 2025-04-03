2fa8b9d23564dbbe2f421448a1dca338b52a3173
supertanuki <supertanuki@gmail.com>
2025-04-03 16:42:37 +0200

    Revert "Remove useless plugin; add debug controls"
    This reverts commit 78da6a0357dd118ab57f273464165cd2f8bb4b6d.

--- src/Game.js
+++ src/Game.js
@@ -73,6 +73,16 @@ export default class Game extends Scene {
     this.tileset = null;
   }
 
+  preload() {
+    console.log("Preload village");
+    this.load.scenePlugin(
+      "AnimatedTiles",
+      "plugins/AnimatedTiles.js",
+      "animatedTiles",
+      "animatedTiles"
+    );
+  }
+
   create() {
     if (urlParamHas("debug")) {
       this.fpsText = this.add
--- src/Village/debugControls.js
+++ src/Village/debugControls.js
@@ -3,15 +3,12 @@ import Game from "../Game";
 import { setVillageForThirdAct, toggleScreensVisibility } from "../Story/afterFactory";
 import { setVillageForSecondAct } from "../Story/afterMineNightmare";
 import { setVillageBeforeMine } from "../Story/mineAccessValidation";
-import { urlParamHas } from "../Utils/isDebug";
 import { switchNight } from "./night";
 import { toggleRoadsVisibility } from "./roads";
 import { secondRiverLessWater } from "./secondRiverLessWater";
 
 /** @param {Game} scene  */
 export const addDebugControls = function (scene) {
-  if (!urlParamHas('debugcontrols')) return;
-
   scene.input.keyboard
     .addKey(Phaser.Input.Keyboard.KeyCodes.F)
     .on("down", () => {
