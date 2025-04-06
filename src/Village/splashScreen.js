f3fe851451dd7b4415140cb266122fe3b4619d1c
supertanuki <supertanuki@gmail.com>
2025-04-06 16:47:38 +0200

    Click on all screen for start

--- src/Village/splashScreen.js
+++ src/Village/splashScreen.js
@@ -16,25 +16,30 @@ export const splashScreen = function (scene) {
       fill: "#ffffff",
     })
     .setOrigin(0.5, 0.5)
-    .setResolution(FONT_RESOLUTION);
-  text.setInteractive({ useHandCursor: true });
-  text.on("pointerdown", () => {
+    .setResolution(FONT_RESOLUTION)
+    .setInteractive({ useHandCursor: true });
+
+  const callback = () => {
+    scene.input.off("pointerdown", callback);
+
     text.disableInteractive(true);
     text.setText("Dans une forêt paisible,\nloin du fracas des villes.");
 
     const loadingText = scene.add
-    .text(225, 220, "Chargement du jeu...", {
-      fontFamily: "DefaultFont",
-      fontSize: FONT_SIZE,
-      fill: "#aaaaaa",
-    })
-    .setOrigin(0.5, 0.5)
-    .setResolution(FONT_RESOLUTION);
+      .text(225, 220, "Chargement...", {
+        fontFamily: "DefaultFont",
+        fontSize: FONT_SIZE,
+        fill: "#aaaaaa",
+      })
+      .setOrigin(0.5, 0.5)
+      .setResolution(FONT_RESOLUTION);
 
     scene.time.delayedCall(500, () => {
+      scene.startGame();
       text.destroy();
       loadingText.destroy();
-      scene.startGame();
     });
-  });
+  };
+
+  scene.input.on("pointerdown", callback);
 };
