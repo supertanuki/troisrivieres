617e347ac04d8c2717f2874ccd8565e8dfe11cf1
supertanuki <supertanuki@gmail.com>
2025-08-05 16:35:19 +0200

    add recycling centre assets

--- src/RecyclingCentre/RecyclingCentre.js
+++ src/RecyclingCentre/RecyclingCentre.js
@@ -10,7 +10,12 @@ import { dispatchUnlockEvents, eventsHas } from "../Utils/events";
 import { FONT_RESOLUTION } from "../UI/Message";
 import { playMiniGameTheme } from "../Utils/music";
 
-const OBJECTS_NAMES = ["console1", "laptop", "phone", "screen"];
+const OBJECTS_NAMES = ["console", "laptop", "phone"];
+const OBJECTS_COLORS = {
+  console: 0xbe4343,
+  laptop: 0x6ec9c6,
+  phone: 0xc09e5a,
+};
 const SPEED_INCREMENT = getUrlParam("speedIncrement", 0.8);
 const initialY = 252;
 const initialX = 200;
@@ -34,93 +39,158 @@ export default class RecyclingCentre extends MiniGameUi {
     this.containers = [];
     this.containersObject = null;
     this.validatedObjects = 0;
-    this.screwFrontPosition = 0;
-    this.screwBackPosition = 0;
     this.conveyorPosition = 0;
     this.speed = SPEED_INCREMENT;
-    this.selectedObject = "console1";
+    this.selectedObject = "console";
     this.warnings = 0;
   }
 
   preload() {
     super.preload();
     this.load.atlas(
-      "recycling",
-      "sprites/recycling.png",
-      "sprites/recycling.json"
+      "recyclingCentre",
+      "sprites/recyclingCentre.png",
+      "sprites/recyclingCentre.json"
     );
-    this.load.image("screw-1", "img/screw-1.png");
-    this.load.image("screw-2", "img/screw-2.png");
   }
 
   create() {
     this.timeStart = Date.now();
     super.create();
-    this.cameras.main.setBackgroundColor(0x444444);
+
+    this.anims.create({
+      key: "trap-open",
+      frames: this.anims.generateFrameNames("recyclingCentre", {
+        start: 1,
+        end: 4,
+        prefix: "trap",
+      }),
+      repeat: 0,
+      frameRate: 10,
+    });
+
+    this.anims.create({
+      key: "trap-close",
+      frames: this.anims.generateFrameNames("recyclingCentre", {
+        start: 4,
+        end: 1,
+        prefix: "trap",
+      }),
+      repeat: 0,
+      frameRate: 10,
+    });
+
+    this.anims.create({
+      key: "shredder-console",
+      frames: this.anims.generateFrameNames("recyclingCentre", {
+        start: 1,
+        end: 2,
+        prefix: "shredder-console-",
+      }),
+      repeat: -1,
+      frameRate: 10,
+    });
+
+    this.anims.create({
+      key: "shredder-phone",
+      frames: this.anims.generateFrameNames("recyclingCentre", {
+        start: 1,
+        end: 2,
+        prefix: "shredder-phone-",
+      }),
+      repeat: -1,
+      frameRate: 10,
+    });
+
+    this.anims.create({
+      key: "shredder-laptop",
+      frames: this.anims.generateFrameNames("recyclingCentre", {
+        start: 1,
+        end: 2,
+        prefix: "shredder-laptop-",
+      }),
+      repeat: -1,
+      frameRate: 10,
+    });
+
     this.scale.setGameSize(550, 300);
     this.matter.world.setBounds(0, -100, 550, 400);
 
-    // bottom zone
-    this.add.rectangle(0, 220, 550, 100, 0x666666).setOrigin(0, 0);
+    this.add.image(275, 150, "recyclingCentre", "background");
+    this.add.image(275, 0, "recyclingCentre", "track").setOrigin(0.5, 0);
 
-    // water
-    this.add.rectangle(30, 220, 50, 150, 0x5cb5e1);
-    this.add.rectangle(520, 220, 50, 150, 0x5cb5e1);
+    this.add.image(26, 300, "recyclingCentre", "water-level").setDepth(1);
+    this.add.image(27, 192, "recyclingCentre", "water-tank").setDepth(1);
+
+    this.add
+      .image(524, 250, "recyclingCentre", "water-level")
+      .setScale(-1, 1)
+      .setDepth(1);
+    this.add
+      .image(522, 192, "recyclingCentre", "water-tank")
+      .setScale(-1, 1)
+      .setDepth(1);
 
     this.objectSource = this.add
-      .rectangle(275, 15, 60, 30, 0x111111)
+      .sprite(275, 0, "recyclingCentre", "trap1")
+      .setOrigin(0.5, 0)
       .setDepth(1);
 
     this.leftCable = this.add.graphics().setDepth(1);
     this.rightCable = this.add.graphics().setDepth(1);
 
+    this.shredder = this.add
+      .sprite(0, -65, "recyclingCentre", "shredder-console-1")
+      .setOrigin(0.5, 0)
+      .setDepth(1);
+    this.shredder.anims.play("shredder-console");
     const containerBase = this.add
-      .rectangle(0, 0, 80, 70, 0x111111)
+      .image(0, 0, "recyclingCentre", "shredder")
+      .setDepth(2);
+
+    const wheelLeft = this.add
+      .image(-50, 45, "recyclingCentre", "wheel")
+      .setDepth(2);
+
+    const wheelRight = this.add
+      .image(50, 45, "recyclingCentre", "wheel")
+      .setDepth(2);
+
+    const buttonLeftTop = this.add
+      .image(-37, 0, "recyclingCentre", "button-red")
       .setDepth(1);
-    this.screwBack = this.add
-      .tileSprite(-40, -35, 80, 19, "screw-2")
-      .setOrigin(0, 1);
-    this.screwFront = this.add
-      .tileSprite(-40, -35, 80, 19, "screw-1")
-      .setOrigin(0, 1)
+    const buttonLeftBottom = this.add
+      .image(-37, 22, "recyclingCentre", "button-red")
       .setDepth(1);
+    const buttonRightTop = this.add
+      .image(37, 0, "recyclingCentre", "button-red")
+      .setDepth(1);
+    const buttonRightBottom = this.add
+      .image(37, 22, "recyclingCentre", "button-red")
+      .setDepth(1);
+
     this.currentObject = this.add
-      .image(0, 0, "recycling", this.selectedObject)
+      .image(0, 2, "recyclingCentre", this.selectedObject)
       .setDepth(1);
     this.previousObject = this.add
-      .image(0, -25, "recycling", "screen")
-      .setScale(0.5)
+      .image(0, -23, "recyclingCentre", "phone-little")
       .setAlpha(0.4);
     this.nextObject = this.add
-      .image(0, 25, "recycling", "laptop")
-      .setScale(0.5)
+      .image(0, 27, "recyclingCentre", "laptop-little")
       .setAlpha(0.4);
 
-    const arrowStyle = {
-      fontFamily: "DefaultFont",
-      fontSize: "16px",
-      fill: "#ffffff",
-    };
-    this.arrowUp = this.add
-      .text(15, -25, "↑", arrowStyle)
-      .setResolution(FONT_RESOLUTION)
-      .setOrigin(0.5, 0.5)
-      .setDepth(1);
-    this.arrowDown = this.add
-      .text(15, 25, "↓", arrowStyle)
-      .setResolution(FONT_RESOLUTION)
-      .setOrigin(0.5, 0.5)
-      .setDepth(1);
-
     this.containersObject = this.add.container(initialX, initialY, [
+      wheelLeft,
+      wheelRight,
+      this.shredder,
+      buttonLeftTop,
+      buttonLeftBottom,
+      buttonRightTop,
+      buttonRightBottom,
       containerBase,
-      this.screwBack,
-      this.screwFront,
       this.currentObject,
       this.previousObject,
       this.nextObject,
-      this.arrowUp,
-      this.arrowDown,
     ]);
 
     this.physics.add.existing(this.containersObject);
@@ -154,6 +224,7 @@ export default class RecyclingCentre extends MiniGameUi {
   gameOver() {
     this.isCinematic = true;
     this.isGameOver = true;
+    this.shredder.anims.stop();
 
     // todo : to remove
     this.updateMessage("Game over");
@@ -237,8 +308,22 @@ export default class RecyclingCentre extends MiniGameUi {
   setSelectedObject(direction) {
     this.selectedObject = this.getObjectNameByDirection(direction);
     this.currentObject.setFrame(this.selectedObject);
-    this.previousObject.setFrame(this.getObjectNameByDirection(-1));
-    this.nextObject.setFrame(this.getObjectNameByDirection(1));
+    this.previousObject.setFrame(this.getObjectNameByDirection(-1) + "-little");
+    this.nextObject.setFrame(this.getObjectNameByDirection(1) + "-little");
+
+    this.tweens.add({
+      targets: this.shredder,
+      y: -30,
+      duration: 100,
+      onComplete: () => {
+        this.shredder.anims.play("shredder-" + this.selectedObject);
+        this.tweens.add({
+          targets: this.shredder,
+          y: -65,
+          duration: 100,
+        });
+      },
+    });
   }
 
   up() {
@@ -251,10 +336,11 @@ export default class RecyclingCentre extends MiniGameUi {
     this.setSelectedObject(1);
   }
 
-  createDebris(x, y) {
+  createDebris(x, y, name) {
+    const color = OBJECTS_COLORS[name];
     Array(Phaser.Math.Between(10, 30))
       .fill(0)
-      .forEach((i) => new RockDebris(this, x, y));
+      .forEach((i) => new ObjectDebris(this, x, y, color));
   }
 
   initObject() {
@@ -272,8 +358,16 @@ export default class RecyclingCentre extends MiniGameUi {
     }
 
     const name = Phaser.Math.RND.pick(OBJECTS_NAMES);
-    const consoleId = Phaser.Math.Between(1, 4);
     const x = Phaser.Math.Between(150, 400);
+
+    if (!this.isTrapAnimating) {
+      this.isTrapAnimating = true;
+      this.objectSource.anims.play("trap-open", true);
+      this.time.delayedCall(1200, () => {
+        this.objectSource.anims.play("trap-close", true);
+        this.isTrapAnimating = false;
+      });
+    }
 
     this.tweens.add({
       ease: "Sine.easeInOut",
@@ -282,15 +376,9 @@ export default class RecyclingCentre extends MiniGameUi {
       duration: 200,
       onComplete: () => {
         const object = this.matter.add.gameObject(
-          this.add.image(
-            x,
-            0,
-            "recycling",
-            `${name}${name === "console" ? consoleId : ""}`
-          )
+          this.add.image(x, 30, "recyclingCentre", name + "-broken")
         );
         this.objects.push(object);
-        //object.setVelocityY(0);
       },
     });
 
@@ -308,27 +396,34 @@ export default class RecyclingCentre extends MiniGameUi {
     const containerX = this.containersObject.x;
 
     this.leftCable.clear();
-    this.leftCable.lineStyle(10, 0x000000);
-    const start = new Phaser.Math.Vector2(50, 250);
-    const end = new Phaser.Math.Vector2(containerX - 35, 250);
-    const control = new Phaser.Math.Vector2((containerX - 10) / 2, 300);
+    this.leftCable.lineStyle(10, 0x060a11);
+    const start = new Phaser.Math.Vector2(70, 250);
+    const end = new Phaser.Math.Vector2(containerX - 45, 250);
+    const control = new Phaser.Math.Vector2((containerX + 20) / 2, 300);
     const curve = new Phaser.Curves.QuadraticBezier(start, control, end);
     curve.draw(this.leftCable);
 
+    const maskGraphicsLeft = this.make.graphics();
+    maskGraphicsLeft.fillRect(70, 0, 500, 300);
+    const maskLeft = maskGraphicsLeft.createGeometryMask();
+    this.leftCable.setMask(maskLeft);
+
     this.rightCable.clear();
-    this.rightCable.lineStyle(10, 0x000000);
-    const startRight = new Phaser.Math.Vector2(500, 250);
-    const endRight = new Phaser.Math.Vector2(containerX + 35, 250);
-    const controlRight = new Phaser.Math.Vector2(
-      (540 + containerX + 80) / 2,
-      300
-    );
+    this.rightCable.lineStyle(10, 0x060a11);
+    const startRight = new Phaser.Math.Vector2(480, 250);
+    const endRight = new Phaser.Math.Vector2(containerX + 45, 250);
+    const controlRight = new Phaser.Math.Vector2((500 + containerX) / 2, 300);
     const curveRight = new Phaser.Curves.QuadraticBezier(
       startRight,
       controlRight,
       endRight
     );
     curveRight.draw(this.rightCable);
+
+    const maskGraphicsRight = this.make.graphics();
+    maskGraphicsRight.fillRect(0, 0, 480, 300);
+    const maskRight = maskGraphicsRight.createGeometryMask();
+    this.rightCable.setMask(maskRight);
   }
 
   moveContainers(stepX) {
@@ -336,18 +431,13 @@ export default class RecyclingCentre extends MiniGameUi {
   }
 
   updateContainers() {
-    this.screwBackPosition++;
-    this.screwBack.setTilePosition(this.screwBackPosition, 0);
-    this.screwFrontPosition--;
-    this.screwFront.setTilePosition(this.screwFrontPosition, 0);
-
     const containerX = this.containersObject.x;
 
     if (this.goingRight) {
-      const realStepX = containerX > 400 ? 0 : STEPX;
+      const realStepX = containerX > 350 ? 0 : STEPX;
       this.moveContainers(realStepX);
     } else if (this.goingLeft) {
-      const realStepX = containerX < 150 ? 0 : STEPX;
+      const realStepX = containerX < 200 ? 0 : STEPX;
       this.moveContainers(-realStepX);
     } else {
       this.containersObject.body.setAccelerationX(0);
@@ -361,7 +451,11 @@ export default class RecyclingCentre extends MiniGameUi {
         continue;
       }
 
-      if (object.y < 190 || object.frame.name !== this.selectedObject) continue;
+      if (
+        object.y < 190 ||
+        object.frame.name !== this.selectedObject + "-broken"
+      )
+        continue;
       if (
         object.x < this.containersObject.x - 45 ||
         object.x > this.containersObject.x + 45
@@ -369,14 +463,14 @@ export default class RecyclingCentre extends MiniGameUi {
         continue;
 
       this.validatedObjects++;
-      this.createDebris(object.x, object.y);
+      this.createDebris(object.x, object.y, this.selectedObject);
       this.objects = this.objects.filter((thisObject) => thisObject !== object);
       object.destroy();
 
       if (this.currentObject.scale === 1) {
         this.tweens.add({
           targets: this.currentObject,
-          scale: 2,
+          scale: 1.4,
           yoyo: 1,
           duration: 100,
         });
@@ -484,13 +578,11 @@ export default class RecyclingCentre extends MiniGameUi {
   }
 }
 
-class RockDebris extends Phaser.GameObjects.Rectangle {
-  constructor(scene, x, y, width, height, color = 0x555555, gravity = false) {
-    color =
-      color |
-      Phaser.Math.RND.pick([0x555555, 0x222222, 0xff0000, 0x00ff00, 0x0000ff]);
-    width = width || Phaser.Math.Between(2, 6);
-    height = height || Phaser.Math.Between(2, 6);
+class ObjectDebris extends Phaser.GameObjects.Rectangle {
+  constructor(scene, x, y, objectColor) {
+    const color = Phaser.Math.RND.pick([0xcccccc, objectColor]);
+    const width = Phaser.Math.Between(2, 6);
+    const height = Phaser.Math.Between(2, 6);
     super(scene, x, y, width, height, color);
     scene.add.existing(this);
     scene.physics.add.existing(this);
