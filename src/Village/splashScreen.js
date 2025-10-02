e948661413272e374714d7be833c7dc7e13a2029
supertanuki <supertanuki@gmail.com>
2025-10-02 16:17:59 +0200

    bonus screen

--- src/Village/splashScreen.js
+++ src/Village/splashScreen.js
@@ -1,6 +1,10 @@
+import Game from "../Game";
+import { goToFactory, goToRecycling } from "../Story/goToGame";
+import { goToMine } from "../Story/goToMine";
 import { FONT_RESOLUTION, FONT_SIZE } from "../UI/Message";
 import { urlParamHas } from "../Utils/debug";
 
+/** @param {Game} scene  */
 export const splashScreen = function (scene) {
   if (urlParamHas("nostart")) {
     scene.resetGameSize();
@@ -8,8 +12,32 @@ export const splashScreen = function (scene) {
     return;
   }
 
-  scene.scale.setGameSize(550, 300)
-  scene.add.image(0, 0, 'splash').setOrigin(0, 0)
+  const isBonus = urlParamHas("bonus");
+  let goingSomewhere = false;
+
+  scene.scale.setGameSize(550, 300);
+  scene.add.image(0, 0, "splash").setOrigin(0, 0);
+
+  const textStart = scene.add
+    .text(275, 164, "Démarrer", {
+      fontFamily: "DefaultFont",
+      fontSize: FONT_SIZE,
+      fill: "#ffffff",
+    })
+    .setOrigin(0.5, 0.5)
+    .setResolution(FONT_RESOLUTION)
+    .setInteractive({ useHandCursor: true });
+
+  const startGame = () => {
+    textStart.disableInteractive(true);
+    textStart.setText("Chargement...");
+
+    scene.time.delayedCall(100, () => {
+      scene.resetGameSize();
+      scene.startGame();
+      textStart.destroy();
+    });
+  };
 
   const title = scene.add
     .text(275, 130, "Trois-Rivières", {
@@ -21,8 +49,36 @@ export const splashScreen = function (scene) {
     .setResolution(FONT_RESOLUTION)
     .setInteractive({ useHandCursor: true });
 
-  const text = scene.add
-    .text(275, 164, "Démarrer", {
+  if (!isBonus) {
+    const callback = () => {
+      if (goingSomewhere) return;
+      goingSomewhere = true;
+      scene.input.off("pointerdown");
+      startGame();
+    };
+    scene.input.on("pointerdown", callback);
+
+    return;
+  }
+
+  title.on("pointerdown", () => {
+    if (goingSomewhere) return;
+    goingSomewhere = true;
+    title.off("pointerdown");
+    textStart.off("pointerdown");
+    startGame();
+  });
+
+  textStart.on("pointerdown", () => {
+    if (goingSomewhere) return;
+    goingSomewhere = true;
+    title.off("pointerdown");
+    textStart.off("pointerdown");
+    startGame();
+  });
+
+  const textMine = scene.add
+    .text(275, 230, "La mine", {
       fontFamily: "DefaultFont",
       fontSize: FONT_SIZE,
       fill: "#ffffff",
@@ -31,18 +87,50 @@ export const splashScreen = function (scene) {
     .setResolution(FONT_RESOLUTION)
     .setInteractive({ useHandCursor: true });
 
-  const callback = () => {
-    scene.input.off("pointerdown", callback);
+  textMine.on("pointerdown", () => {
+    if (goingSomewhere) return;
+    goingSomewhere = true;
+    textMine.disableInteractive(true);
+    textMine.setColor("#000000");
+    textMine.off("pointerdown");
+    goToMine(scene);
+  });
 
-    text.disableInteractive(true);
-    text.setText("Chargement...");
+  const textFactory = scene.add
+    .text(275, 250, "L'usine", {
+      fontFamily: "DefaultFont",
+      fontSize: FONT_SIZE,
+      fill: "#ffffff",
+    })
+    .setOrigin(0.5, 0.5)
+    .setResolution(FONT_RESOLUTION)
+    .setInteractive({ useHandCursor: true });
 
-    scene.time.delayedCall(100, () => {
-      scene.resetGameSize();
-      scene.startGame();
-      text.destroy();
-    });
-  };
+  textFactory.on("pointerdown", () => {
+    if (goingSomewhere) return;
+    goingSomewhere = true;
+    textFactory.disableInteractive(true);
+    textFactory.setColor("#000000");
+    textFactory.off("pointerdown");
+    goToFactory(scene);
+  });
 
-  scene.input.on("pointerdown", callback);
+  const textRecycling = scene.add
+    .text(275, 270, "Le recyclage", {
+      fontFamily: "DefaultFont",
+      fontSize: FONT_SIZE,
+      fill: "#ffffff",
+    })
+    .setOrigin(0.5, 0.5)
+    .setResolution(FONT_RESOLUTION)
+    .setInteractive({ useHandCursor: true });
+
+  textRecycling.on("pointerdown", () => {
+    if (goingSomewhere) return;
+    goingSomewhere = true;
+    textRecycling.disableInteractive(true);
+    textRecycling.setColor("#000000");
+    textRecycling.off("pointerdown");
+    goToRecycling(scene);
+  });
 };
