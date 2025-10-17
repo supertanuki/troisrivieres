a51d6d5dabaab39ce386aacd46124363dea6dee5
supertanuki <supertanuki@gmail.com>
2025-10-17 15:39:28 +0200

    bike positions

--- src/Sprites/Bike.js
+++ src/Sprites/Bike.js
@@ -1,9 +1,11 @@
 import Phaser from "phaser";
 import Chat from "../UI/Chat";
+import { randomSign } from "../Utils/randomSign";
 
 export default class Bike extends Chat {
   constructor(scene, x, y) {
     super(scene, x, y, "sprites", "bike", 0, 0, true);
+    this.scaleX = -1 * randomSign();
   }
 }
 
@@ -16,6 +18,7 @@ Phaser.GameObjects.GameObjectFactory.register("bike", function (x, y) {
   );
 
   sprite.body.setSize(sprite.width, 1);
+  if (sprite.scaleX === -1) sprite.setOffset(sprite.width, sprite.height/2);
   sprite.setImmovable(true);
 
   this.displayList.add(sprite);
