fc7e31a18d2cd93b4dc711213f218f43ab183a00
supertanuki <supertanuki@gmail.com>
2025-12-13 14:24:01 +0100

    splash screen with locale selection

--- src/Village/splashScreen.js
+++ src/Village/splashScreen.js
@@ -17,90 +17,208 @@ export const splashScreen = function (scene) {
     return;
   }
 
-  scene.isBonus = urlParamHas("bonus");
-  if (scene.isBonus) {
-    afterLocaleSelected(scene);
-    return;
-  }
+  const afterLocaleSelected = () => {
+    let goingSomewhere = false;
+    let continueplay = null;
+    const progression = getProgression();
 
-  if (isMobileOrTablet()) {
-    const orientationMessage = scene.add
+    const textStart = scene.add
       .bitmapText(
-        225,
-        50,
+        275,
+        164,
         "FreePixel-16",
-        "Tournez votre appareil en mode paysage.",
-        32
+        scene.isBonus || progression
+          ? getUiMessage("game.newGame")
+          : getUiMessage("game.play"),
+        16
       )
-      .setMaxWidth(400)
       .setOrigin(0.5, 0.5)
-      .setVisible(false)
-      .setDepth(10000);
+      .setTintFill(0xffffff)
+      .setInteractive({ useHandCursor: true });
 
-    const checkOrientation = () => {
-      if (
-        screen?.orientation?.type?.includes("landscape") ||
-        (!screen?.orientation &&
-          window?.matchMedia("(orientation: landscape)")?.matches)
-      ) {
-        orientationMessage.setVisible(false);
-        return;
+    textStart.input.hitArea.setTo(
+      -20,
+      -10,
+      textStart.width + 40,
+      textStart.height + 20
+    );
+
+    const startGame = (continueplaying = false) => {
+      scene.isBonus = false;
+      goingSomewhere = true;
+
+      if (continueplaying) {
+        continueplay.disableInteractive(true);
+        continueplay.setText(getUiMessage("game.loading"));
+        textStart.destroy();
+      } else {
+        textStart.disableInteractive(true);
+        textStart.setText(getUiMessage("game.loading"));
+        continueplay?.destroy();
       }
 
-      orientationMessage.setVisible(true);
+      document.body.style.cursor = "none";
+
+      scene.cameras.main.fadeOut(500, 0, 0, 0, (cam, progress) => {
+        if (progress !== 1) return;
+
+        scene.resetGameSize();
+        scene.startGame(continueplaying);
+        colorBackground.destroy();
+        textStart?.destroy();
+        continueplay?.destroy();
+        title.destroy();
+        mai.destroy();
+        rock.destroy();
+        background.destroy();
+        cloudTop.destroy();
+        cloudBig.destroy();
+        cloudTwo.destroy();
+        cloudMiddle.destroy();
+        smokeBig.destroy();
+        smokeSmall.destroy();
+      });
     };
 
-    checkOrientation();
-    if (screen?.orientation) {
-      screen.orientation.addEventListener("change", () => checkOrientation());
-    } else {
-      const mql = window?.matchMedia("(orientation: landscape)");
-      mql?.addListener((m) => {
-        if (m.matches) orientationMessage.setVisible(false);
-        else orientationMessage.setVisible(true);
+    const clickBonusGame = (textObject) => {
+      goingSomewhere = true;
+      textObject.disableInteractive(true);
+      textObject.setTintFill(0x333333);
+      document.body.style.cursor = "none";
+    };
+
+    textStart.on("pointerdown", () => {
+      if (goingSomewhere) return;
+      startGame();
+    });
+    textStart.on("pointerover", () => mouseOver(textStart, 0x307f6d));
+    textStart.on("pointerout", () => mouseOut(textStart));
+
+    if (progression) {
+      continueplay = scene.add
+        .bitmapText(
+          275,
+          124,
+          "FreePixel-16",
+          getUiMessage("game.continueGame"),
+          16
+        )
+        .setOrigin(0.5, 0.5)
+        .setTintFill(0xffffff)
+        .setInteractive({ useHandCursor: true });
+
+      continueplay.input.hitArea.setTo(
+        -20,
+        -10,
+        continueplay.width + 40,
+        continueplay.height + 20
+      );
+
+      continueplay.on("pointerdown", () => {
+        if (goingSomewhere) return;
+        startGame(true);
+      });
+
+      continueplay.on("pointerover", () => mouseOver(continueplay, 0x307f6d));
+      continueplay.on("pointerout", () => mouseOut(continueplay));
+    }
+
+    const credits = scene.add
+      .bitmapText(510, 285, "FreePixel-16", getUiMessage("game.credits"), 16)
+      .setOrigin(0.5, 0.5)
+      .setTintFill(0xffffff)
+      .setInteractive({ useHandCursor: true });
+
+    credits.input.hitArea.setTo(
+      -20,
+      -10,
+      credits.width + 40,
+      credits.height + 20
+    );
+    credits.on("pointerdown", () => (window.location.href = "credits.html"));
+    credits.on("pointerover", () => mouseOver(credits, 0x000000));
+    credits.on("pointerout", () => mouseOut(credits));
+
+    if (scene.isBonus) {
+      const textMine = scene.add
+        .bitmapText(160, 210, "FreePixel-16", getUiMessage("game.mine"), 16)
+        .setOrigin(0.5, 0.5)
+        .setTintFill(0xffffff)
+        .setInteractive({ useHandCursor: true });
+      textMine.input.hitArea.setTo(
+        -20,
+        -10,
+        textMine.width + 40,
+        textMine.height + 20
+      );
+      textMine.on("pointerover", () => mouseOver(textMine));
+      textMine.on("pointerout", () => mouseOut(textMine));
+      textMine.on("pointerdown", () => {
+        if (goingSomewhere) return;
+        clickBonusGame(textMine);
+        goToMine(scene);
+      });
+
+      const textFactory = scene.add
+        .bitmapText(260, 240, "FreePixel-16", getUiMessage("game.factory"), 16)
+        .setOrigin(0.5, 0.5)
+        .setTintFill(0xffffff)
+        .setInteractive({ useHandCursor: true });
+      textFactory.input.hitArea.setTo(
+        -20,
+        -10,
+        textFactory.width + 40,
+        textFactory.height + 20
+      );
+      textFactory.on("pointerover", () => mouseOver(textFactory));
+      textFactory.on("pointerout", () => mouseOut(textFactory));
+      textFactory.on("pointerdown", () => {
+        if (goingSomewhere) return;
+        clickBonusGame(textFactory);
+        goToFactory(scene);
+      });
+
+      const textRecycling = scene.add
+        .bitmapText(
+          400,
+          260,
+          "FreePixel-16",
+          getUiMessage("game.recycling"),
+          16
+        )
+        .setOrigin(0.5, 0.5)
+        .setTintFill(0xffffff)
+        .setInteractive({ useHandCursor: true });
+      textRecycling.input.hitArea.setTo(
+        -20,
+        -10,
+        textRecycling.width + 40,
+        textRecycling.height + 20
+      );
+      textRecycling.on("pointerover", () => mouseOver(textRecycling));
+      textRecycling.on("pointerout", () => mouseOut(textRecycling));
+      textRecycling.on("pointerdown", () => {
+        if (goingSomewhere) return;
+        clickBonusGame(textRecycling);
+        goToRecycling(scene);
       });
     }
-  }
 
-  const chooseLocale = (locale) => {
-    saveLocale(locale);
-    frText.destroy();
-    enText.destroy();
-    afterLocaleSelected(scene);
+    sceneEventsEmitter.on(sceneEvents.EventsUnlocked, (data) => {
+      if (!scene.isBonus) return;
+
+      if (
+        eventsHas(data, "factory_after") ||
+        eventsHas(data, "mine_after") ||
+        eventsHas(data, "recycling_after")
+      ) {
+        window.location.href = "?bonus";
+      }
+    });
   };
 
-  const frText = scene.add
-    .bitmapText(150, 125, "FreePixel-16", "Français", 16)
-    .setOrigin(0.5, 0.5)
-    .setTintFill(0xffffff)
-    .setInteractive({ useHandCursor: true });
-
-  frText.input.hitArea.setTo(-30, -30, frText.width + 60, frText.height + 60);
-
-  frText.on("pointerdown", () => chooseLocale("fr"));
-  frText.on("pointerover", () => mouseOver(frText, 0x307f6d));
-  frText.on("pointerout", () => mouseOut(frText));
-
-  const enText = scene.add
-    .bitmapText(300, 125, "FreePixel-16", "English", 16)
-    .setOrigin(0.5, 0.5)
-    .setTintFill(0xffffff)
-    .setInteractive({ useHandCursor: true });
-
-  enText.input.hitArea.setTo(-30, -30, enText.width + 60, enText.height + 60);
-
-  enText.on("pointerdown", () => chooseLocale("en"));
-  enText.on("pointerover", () => mouseOver(enText, 0x307f6d));
-  enText.on("pointerout", () => mouseOut(enText));
-};
-
-const afterLocaleSelected = function (scene) {
   scene.cameras.main.fadeIn(1000);
   const progression = getProgression();
-
-  let goingSomewhere = false;
-  let continueplay = null;
-
   scene.scale.setGameSize(550, 300);
   playVillageAmbiance(scene);
 
@@ -233,193 +351,81 @@ const afterLocaleSelected = function (scene) {
     .setOrigin(0, 0);
   smokeSmall.play("smoke-small");
 
-  const textStart = scene.add
-    .bitmapText(
-      275,
-      164,
-      "FreePixel-16",
-      scene.isBonus || progression
-        ? getUiMessage("game.newGame")
-        : getUiMessage("game.play"),
-      16
-    )
-    .setOrigin(0.5, 0.5)
-    .setTintFill(0xffffff)
-    .setInteractive({ useHandCursor: true });
-
-  textStart.input.hitArea.setTo(
-    -20,
-    -10,
-    textStart.width + 40,
-    textStart.height + 20
-  );
-
-  const startGame = (continueplaying = false) => {
-    scene.isBonus = false;
-    goingSomewhere = true;
-
-    if (continueplaying) {
-      continueplay.disableInteractive(true);
-      continueplay.setText(getUiMessage("game.loading"));
-      textStart.destroy();
-    } else {
-      textStart.disableInteractive(true);
-      textStart.setText(getUiMessage("game.loading"));
-      continueplay?.destroy();
-    }
-
-    document.body.style.cursor = "none";
-
-    scene.cameras.main.fadeOut(500, 0, 0, 0, (cam, progress) => {
-      if (progress !== 1) return;
-
-      scene.resetGameSize();
-      scene.startGame(continueplaying);
-      colorBackground.destroy();
-      textStart?.destroy();
-      continueplay?.destroy();
-      title.destroy();
-      mai.destroy();
-      rock.destroy();
-      background.destroy();
-      cloudTop.destroy();
-      cloudBig.destroy();
-      cloudTwo.destroy();
-      cloudMiddle.destroy();
-      smokeBig.destroy();
-      smokeSmall.destroy();
-    });
-  };
-
-  const clickBonusGame = (textObject) => {
-    goingSomewhere = true;
-    textObject.disableInteractive(true);
-    textObject.setTintFill(0x333333);
-    document.body.style.cursor = "none";
-  };
-
-  textStart.on("pointerdown", () => {
-    if (goingSomewhere) return;
-    startGame();
-  });
-  textStart.on("pointerover", () => mouseOver(textStart, 0x307f6d));
-  textStart.on("pointerout", () => mouseOut(textStart));
-
-  if (progression) {
-    continueplay = scene.add
+  if (isMobileOrTablet()) {
+    const orientationMessage = scene.add
       .bitmapText(
-        275,
-        124,
+        225,
+        50,
         "FreePixel-16",
-        getUiMessage("game.continueGame"),
-        16
+        "Tournez votre appareil en mode paysage.",
+        32
       )
+      .setMaxWidth(400)
       .setOrigin(0.5, 0.5)
-      .setTintFill(0xffffff)
-      .setInteractive({ useHandCursor: true });
+      .setVisible(false)
+      .setDepth(10000);
 
-    continueplay.input.hitArea.setTo(
-      -20,
-      -10,
-      continueplay.width + 40,
-      continueplay.height + 20
-    );
+    const checkOrientation = () => {
+      if (
+        screen?.orientation?.type?.includes("landscape") ||
+        (!screen?.orientation &&
+          window?.matchMedia("(orientation: landscape)")?.matches)
+      ) {
+        orientationMessage.setVisible(false);
+        return;
+      }
 
-    continueplay.on("pointerdown", () => {
-      if (goingSomewhere) return;
-      startGame(true);
-    });
+      orientationMessage.setVisible(true);
+    };
 
-    continueplay.on("pointerover", () => mouseOver(continueplay, 0x307f6d));
-    continueplay.on("pointerout", () => mouseOut(continueplay));
+    checkOrientation();
+    if (screen?.orientation) {
+      screen.orientation.addEventListener("change", () => checkOrientation());
+    } else {
+      const mql = window?.matchMedia("(orientation: landscape)");
+      mql?.addListener((m) => {
+        if (m.matches) orientationMessage.setVisible(false);
+        else orientationMessage.setVisible(true);
+      });
+    }
   }
 
-  const credits = scene.add
-    .bitmapText(510, 285, "FreePixel-16", getUiMessage("game.credits"), 16)
+  scene.isBonus = urlParamHas("bonus");
+  if (scene.isBonus) {
+    afterLocaleSelected();
+    return;
+  }
+
+  const chooseLocale = (locale) => {
+    saveLocale(locale);
+    frText.destroy();
+    enText.destroy();
+    afterLocaleSelected();
+  };
+
+  const frText = scene.add
+    .bitmapText(220, 160, "FreePixel-16", "Français", 16)
     .setOrigin(0.5, 0.5)
     .setTintFill(0xffffff)
     .setInteractive({ useHandCursor: true });
 
-  credits.input.hitArea.setTo(
-    -20,
-    -10,
-    credits.width + 40,
-    credits.height + 20
-  );
-  credits.on("pointerdown", () => (window.location.href = "credits.html"));
-  credits.on("pointerover", () => mouseOver(credits, 0x000000));
-  credits.on("pointerout", () => mouseOut(credits));
+  frText.input.hitArea.setTo(-20, -20, frText.width + 40, frText.height + 40);
 
-  if (scene.isBonus) {
-    const textMine = scene.add
-      .bitmapText(160, 210, "FreePixel-16", getUiMessage("game.mine"), 16)
-      .setOrigin(0.5, 0.5)
-      .setTintFill(0xffffff)
-      .setInteractive({ useHandCursor: true });
-    textMine.input.hitArea.setTo(
-      -20,
-      -10,
-      textMine.width + 40,
-      textMine.height + 20
-    );
-    textMine.on("pointerover", () => mouseOver(textMine));
-    textMine.on("pointerout", () => mouseOut(textMine));
-    textMine.on("pointerdown", () => {
-      if (goingSomewhere) return;
-      clickBonusGame(textMine);
-      goToMine(scene);
-    });
+  frText.on("pointerdown", () => chooseLocale("fr"));
+  frText.on("pointerover", () => mouseOver(frText, 0x307f6d));
+  frText.on("pointerout", () => mouseOut(frText));
 
-    const textFactory = scene.add
-      .bitmapText(260, 240, "FreePixel-16", getUiMessage("game.factory"), 16)
-      .setOrigin(0.5, 0.5)
-      .setTintFill(0xffffff)
-      .setInteractive({ useHandCursor: true });
-    textFactory.input.hitArea.setTo(
-      -20,
-      -10,
-      textFactory.width + 40,
-      textFactory.height + 20
-    );
-    textFactory.on("pointerover", () => mouseOver(textFactory));
-    textFactory.on("pointerout", () => mouseOut(textFactory));
-    textFactory.on("pointerdown", () => {
-      if (goingSomewhere) return;
-      clickBonusGame(textFactory);
-      goToFactory(scene);
-    });
+  const enText = scene.add
+    .bitmapText(330, 160, "FreePixel-16", "English", 16)
+    .setOrigin(0.5, 0.5)
+    .setTintFill(0xffffff)
+    .setInteractive({ useHandCursor: true });
 
-    const textRecycling = scene.add
-      .bitmapText(400, 260, "FreePixel-16", getUiMessage("game.recycling"), 16)
-      .setOrigin(0.5, 0.5)
-      .setTintFill(0xffffff)
-      .setInteractive({ useHandCursor: true });
-    textRecycling.input.hitArea.setTo(
-      -20,
-      -10,
-      textRecycling.width + 40,
-      textRecycling.height + 20
-    );
-    textRecycling.on("pointerover", () => mouseOver(textRecycling));
-    textRecycling.on("pointerout", () => mouseOut(textRecycling));
-    textRecycling.on("pointerdown", () => {
-      if (goingSomewhere) return;
-      clickBonusGame(textRecycling);
-      goToRecycling(scene);
-    });
-  }
+  enText.input.hitArea.setTo(-20, -20, enText.width + 40, enText.height + 40);
 
-  sceneEventsEmitter.on(sceneEvents.EventsUnlocked, (data) => {
-    if (!scene.isBonus) return;
-
-    if (
-      eventsHas(data, "factory_after") ||
-      eventsHas(data, "mine_after") ||
-      eventsHas(data, "recycling_after")
-    ) {
-      window.location.href = "?bonus";
-    }
-  });
+  enText.on("pointerdown", () => chooseLocale("en"));
+  enText.on("pointerover", () => mouseOver(enText, 0x307f6d));
+  enText.on("pointerout", () => mouseOut(enText));
 };
 
 const mouseOver = (target, color = 0x000000) => target.setTintFill(color);
